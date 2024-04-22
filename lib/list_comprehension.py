499b7a4d8564dd7d06edd05073a23762a7a8a163
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-22 14:01:41 -0700

    complete list comprehension lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    even_elements = [num for num in num_list if num % 2 == 0]
+    return even_elements
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [sentence + "!" for sentence in sentence_list]
+    return exclamations
